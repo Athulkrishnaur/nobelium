41cfbf09274be223b87edd524d88eef9ce8506df
athul <athulak.aa@gmail.com>
2023-03-21 15:17:00 +0530

    intial comit

---
+++ src/components/Navbar.js
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+// import { a } from 'react-router-dom'
+import {  GiHamburgerMenu } from "react-icons/gi"
+
+import './Navbar.css'
+
+const Navbar = () => {
+  const [showNavbar, setShowNavbar] = useState(false)
+
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar)
+  }
+
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <div className="logo">
+          < h1>
+          FUNCTION UP
+          </h1>
+        </div>
+        <div className="menu-icon" onClick={handleShowNavbar}>
+          <GiHamburgerMenu />
+        </div>
+        <div className={`nav-elements  ${showNavbar && 'active'}`}>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/blog">Blog</a>
+            </li>
+            <li>
+              <a href="/projects">Projects</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
